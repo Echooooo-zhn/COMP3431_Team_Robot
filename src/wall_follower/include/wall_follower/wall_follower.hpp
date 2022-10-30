f7ae2570cb5c8c997549b9c7770e2c44dcbedb3c
Rodney Boyce <rodney-boyce@hotmail.com>
2022-10-30 07:25:14 -0700

    Split the left ranges into 3 parts for more accurate limits/boudning, also cleaned up the logging output by removing the useless info and only printing the a move once

--- src/wall_follower/include/wall_follower/wall_follower.hpp
+++ src/wall_follower/include/wall_follower/wall_follower.hpp
@@ -23,17 +23,20 @@
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <math.h>
 
 #define DEG2RAD (M_PI / 180.0)
 #define RAD2DEG (180.0 / M_PI)
 
 #define FRONT 0
-#define LEFT 1
-#define RIGHT 2
-#define BACK  3
+#define FLEFT 1
+#define MLEFT 2
+#define LLEFT 3
+#define RIGHT 4
+#define BACK  5
 
-#define LINEAR_VELOCITY  0.15
-#define ANGULAR_VELOCITY 0.3
+#define LINEAR_VELOCITY  0.2 // working 0.15
+#define ANGULAR_VELOCITY 0.6 // working 0.3
 
 #define NUM_SCANS 360
 
@@ -52,7 +55,17 @@ private:
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
 
   // Variables
-  double scan_ranges[4];
+  double scan_ranges[6];
+
+  double forward_dist_limit = 0.5;
+	double fleft_limit = (sin(DEG2RAD * 30) * forward_dist_limit) / sin(DEG2RAD * 120) - 0.01;
+	double mleft_limit = (sin(DEG2RAD * 30) * forward_dist_limit) / sin(DEG2RAD * 105) - 0.01;
+	double lleft_limit = (sin(DEG2RAD * 30) * forward_dist_limit) / sin(DEG2RAD * 90) - 0.01;
+  double side_dist_limit = lleft_limit;
+	double window_width = 0.1;
+
+  bool debug = true;
+  int debug_state = 0;
 
   // ROS timer
   rclcpp::TimerBase::SharedPtr update_timer_;
@@ -62,5 +75,9 @@ private:
   void update_cmd_vel(double linear, double angular);
   void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+  bool left_close();
+  bool left_far();
+  bool right_close();
+  bool front_close();
 };
 #endif  // TURTLEBOT3_GAZEBO__TURTLEBOT3_DRIVE_HPP_
--- src/wall_follower/src/wall_follower.cpp
+++ src/wall_follower/src/wall_follower.cpp
@@ -58,12 +58,12 @@ WallFollower::WallFollower()
 	************************************************************/
 	update_timer_ = this->create_wall_timer(10ms, std::bind(&WallFollower::update_callback, this));
 
-	RCLCPP_INFO(this->get_logger(), "Wall follower node has been initialised");
+	RCLCPP_INFO(this->get_logger(), "\33[2K\rWall follower node has been initialised");
 }
 
 WallFollower::~WallFollower()
 {
-	RCLCPP_INFO(this->get_logger(), "Wall follower node has been terminated");
+	RCLCPP_INFO(this->get_logger(), "\33[2K\rWall follower node has been terminated");
 }
 
 /********************************************************************************
@@ -84,8 +84,6 @@ void WallFollower::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 
 void WallFollower::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
 {
-	RCLCPP_INFO(this->get_logger(), "Num scans: %d", msg->ranges.size());
-
 	// Setting up scan data 
 	// Creating vectors to store a range of scan data
 	std::vector<float>::const_iterator it_first = msg->ranges.begin();
@@ -99,6 +97,25 @@ void WallFollower::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr ms
 	it_first = msg->ranges.begin() + 45;
 	it_last = msg->ranges.begin() + 85;
 	std::vector<float> left_ranges(it_first, it_last);
+	// We split the left ranges into 3 sections for a more accuracy
+	// For example if we find the minimum of the data scanned at 45 to 85 degrees,
+	// and since the bot is a square we know that the distance from the edge of the
+	// turtlebot to the wall at 85 degrees is shorter than 45 degrees. So in the
+	// case that the minimum value in the range is at 45 degrees the bot will closer 
+	// than the intended side limit before it turns. This is simply fixed by splitting
+	// the ranges in as many parts such that the difference between the start and end
+	// degrees is negligible. For maximum effectiveness each degree would have their own
+	// side limit however this is not necessary as only a few splits would be more than
+	// enough for the difference to become negligible.
+	it_first = left_ranges.begin();
+	it_last = left_ranges.begin() + 13;
+	std::vector<float> fleft_ranges(it_first, it_last);
+	it_first = left_ranges.begin() + 14;
+	it_last = left_ranges.begin() + 27;
+	std::vector<float> mleft_ranges(it_first, it_last);
+	it_first = left_ranges.begin() + 28;
+	it_last = left_ranges.begin() + 40;
+	std::vector<float> lleft_ranges(it_first, it_last);
 
 	it_first = msg->ranges.begin() + 120;
 	it_last = msg->ranges.begin() + 240;
@@ -109,11 +126,13 @@ void WallFollower::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr ms
 	std::vector<float> right_ranges(it_first, it_last);
 
 	scan_ranges[FRONT] = *std::min_element(forward_ranges.begin(), forward_ranges.end());
-	scan_ranges[LEFT] = *std::min_element(left_ranges.begin(), left_ranges.end());
+	scan_ranges[FLEFT] = *std::min_element(fleft_ranges.begin(), fleft_ranges.end());
+	scan_ranges[MLEFT] = *std::min_element(mleft_ranges.begin(), mleft_ranges.end());
+	scan_ranges[LLEFT] = *std::min_element(lleft_ranges.begin(), lleft_ranges.end());
 	scan_ranges[RIGHT] = *std::min_element(right_ranges.begin(), right_ranges.end());
 	scan_ranges[BACK] = *std::min_element(back_ranges.begin(), back_ranges.end());
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		if (std::isinf(scan_ranges[i]))
 		{
@@ -136,36 +155,59 @@ void WallFollower::update_cmd_vel(double linear, double angular)
 ********************************************************************************/
 void WallFollower::update_callback()
 {
-	double forward_dist_limit = 0.5;
-	double side_dist_limit = 0.24;
-	double window_width = 0.1;
+	
 
 	// Check if there is space in front 
-	RCLCPP_INFO(this->get_logger(), "Forward %lf", scan_ranges[FRONT]);
-	if (scan_ranges[FRONT] > forward_dist_limit || scan_ranges[FRONT] == 0) {
-		if (scan_ranges[LEFT] < side_dist_limit && (scan_ranges[RIGHT] > side_dist_limit || scan_ranges[RIGHT] == 0)) {
+	if (front_close()) {
+		if (left_close() && !right_close()) {
 			// Too close to left wall, turning right
-			RCLCPP_INFO(this->get_logger(), "Too close to left wall");
-			RCLCPP_INFO(this->get_logger(), "%lf < %lf", scan_ranges[LEFT], side_dist_limit);
+			if (debug && debug_state != 1) {
+				double min = 5.0;
+				for (int i = FLEFT; i <= LLEFT; i++) {
+					if (scan_ranges[i] < min) {
+						min = scan_ranges[i];
+					}
+				}
+				double limit = 0.0;
+				if (min == scan_ranges[FLEFT]) {
+					limit = fleft_limit;
+				} else if (min == scan_ranges[MLEFT]) {
+					limit = mleft_limit;
+				} else {
+					limit = lleft_limit;
+				}
+				RCLCPP_INFO(this->get_logger(), "\33[2K\rToo close to the left wall:  %lf < %lf", min, limit);
+				debug_state = 1;
+			}
 			update_cmd_vel(LINEAR_VELOCITY, -1 * ANGULAR_VELOCITY);
-		} else if (scan_ranges[LEFT] > (side_dist_limit + window_width) || scan_ranges[LEFT] == 0) {
+		} else if (left_far()) {
 			// Too far from left wall, turning left
-			RCLCPP_INFO(this->get_logger(), "Too far from left wall");
-			RCLCPP_INFO(this->get_logger(), "Left %lf > %lf", scan_ranges[LEFT], forward_dist_limit + window_width);
-			auto speed_reduction = side_dist_limit / scan_ranges[LEFT];
-			update_cmd_vel(LINEAR_VELOCITY * speed_reduction, ANGULAR_VELOCITY);
-		} else if (scan_ranges[RIGHT] < side_dist_limit && (scan_ranges[LEFT] > side_dist_limit || scan_ranges[LEFT] == 0)) {
+			if (debug && debug_state != 2) {
+				RCLCPP_INFO(this->get_logger(), "\33[2K\rToo far from left wall:  %lf > %lf", scan_ranges[FLEFT], fleft_limit + window_width);
+				debug_state = 2;
+			}
+			update_cmd_vel(LINEAR_VELOCITY, ANGULAR_VELOCITY );
+		} else if (right_close() && !left_close()) {
 			// Too close to right wall, turning left
-			RCLCPP_INFO(this->get_logger(), "Too close to right wall");
+			if (debug && debug_state != 3) {
+				RCLCPP_INFO(this->get_logger(), "\33[2K\rToo close to the right wall:  %lf < %lf", scan_ranges[RIGHT], side_dist_limit);
+				debug_state = 3;
+			}
 			update_cmd_vel(LINEAR_VELOCITY, ANGULAR_VELOCITY);
-			RCLCPP_INFO(this->get_logger(), "Right %lf < %lf", scan_ranges[RIGHT], side_dist_limit);
 		} else {
 			// If we're not too close to anything, but there is enough space infront, go forward
-			RCLCPP_INFO(this->get_logger(), "Moving Forward");
+			if (debug && debug_state != 4) {
+				RCLCPP_INFO(this->get_logger(), "\33[2K\rMoving Forward:  %lf", scan_ranges[FRONT]);
+				debug_state = 4;
+			}
 			update_cmd_vel(LINEAR_VELOCITY, 0);
 		}
 	} else {
 		// No space infront
+		if (debug && debug_state != 5) {
+			RCLCPP_INFO(this->get_logger(), "\33[2K\rForward blocked, turning right:  %lf < %lf", scan_ranges[FRONT], forward_dist_limit);
+			debug_state = 5;
+		}
 		/*
 		// This code currently doesnt work for this implementation more logic is needed.
 
@@ -173,27 +215,47 @@ void WallFollower::update_callback()
 			// Check if we can turn left
 			auto speed_reduction = (scan_ranges[FRONT] - (forward_dist_limit * 0.6)) / forward_dist_limit;
 			update_cmd_vel(LINEAR_VELOCITY * speed_reduction, ANGULAR_VELOCITY);
-			RCLCPP_INFO(this->get_logger(), "Turning Left: %lf > %lf", scan_ranges[LEFT], (side_dist_limit + window_width));
+			RCLCPP_INFO(this->get_logger(), "\33[2KTurning Left: %lf > %lf", scan_ranges[LEFT], (side_dist_limit + window_width));
 		} else if (scan_ranges[RIGHT] > side_dist_limit || scan_ranges[RIGHT] == 0) {
 			// Check if we can turn right
 			auto speed_reduction = (scan_ranges[FRONT] - (forward_dist_limit * 0.6)) / forward_dist_limit;
 			update_cmd_vel(LINEAR_VELOCITY * speed_reduction, -1 * ANGULAR_VELOCITY);
-			RCLCPP_INFO(this->get_logger(), "Turning Right");
+			RCLCPP_INFO(this->get_logger(), "\33[2KTurning Right");
 		} else if (scan_ranges[BACK] > side_dist_limit || scan_ranges[BACK] == 0) {
 			// Check if we can turn around
 			update_cmd_vel(0, -1 * ANGULAR_VELOCITY);
-			RCLCPP_INFO(this->get_logger(), "U TURN");
+			RCLCPP_INFO(this->get_logger(), "\33[2KU TURN");
 		} else {
 			// STUCK
 			update_cmd_vel(0, 0);
-			RCLCPP_INFO(this->get_logger(), "STUCK");
+			RCLCPP_INFO(this->get_logger(), "\33[2KSTUCK");
 		}
 		*/
 		// Turn right whilst slowing linear speed based on formula below
-		auto speed_reduction = (scan_ranges[FRONT] - (forward_dist_limit * 0.6)) / forward_dist_limit;
+		
+		auto speed_reduction = (scan_ranges[FRONT] - (forward_dist_limit * 0.5)) / forward_dist_limit;
 		update_cmd_vel(LINEAR_VELOCITY * speed_reduction, -1 * ANGULAR_VELOCITY);
-		RCLCPP_INFO(this->get_logger(), "Turning Right");
 	}
+}
+
+bool WallFollower::left_close() {
+	return scan_ranges[LLEFT] < lleft_limit ||
+			scan_ranges[MLEFT] < mleft_limit ||
+			scan_ranges[FLEFT] < fleft_limit;
+}
+
+bool WallFollower::left_far() {
+	return scan_ranges[LLEFT] >= lleft_limit + window_width ||
+			scan_ranges[MLEFT] >= mleft_limit + window_width ||
+			scan_ranges[FLEFT] >= fleft_limit + window_width;
+}
+
+bool WallFollower::right_close() {
+	return scan_ranges[RIGHT] < side_dist_limit;
+}
+
+bool WallFollower::front_close() {
+	return scan_ranges[FRONT] > forward_dist_limit;
 }
 
 /*******************************************************************************
